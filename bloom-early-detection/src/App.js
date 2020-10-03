999e4f1645e2cdcc93fdb2b725bc74bc73388f30
Aniket Pant <aniketpant@jefcoed.com>
2020-10-03 13:28:51 -0400

    Added control panel boiler

--- bloom-early-detection/src/App.js
+++ bloom-early-detection/src/App.js
@@ -1,7 +1,10 @@
 import React from "react";
+import "antd/dist/antd.css";
 import { StaticMap } from "react-map-gl";
 import DeckGL, { GeoJsonLayer, ArcLayer } from "deck.gl";
 import "./App.css";
+import { Typography } from "antd";
+const { Title, Text } = Typography;
 
 const MAPBOX_TOKEN =
   "pk.eyJ1IjoicGFudDIwMDIiLCJhIjoiY2prenlwb2ZtMHlnMjNxbW1ld3VxYWZ4cCJ9.rOb8DhCzsysBIw69MxyWKg"; // eslint-disable-line
@@ -16,6 +19,15 @@ const INITIAL_VIEW_STATE = {
   bearing: 0,
   pitch: 30,
 };
+
+const heatmapColorRange = [
+  [1, 152, 189],
+  [73, 227, 206],
+  [216, 254, 181],
+  [254, 237, 177],
+  [254, 173, 84],
+  [209, 55, 78],
+];
 
 function App() {
   const onClick = (info) => {
@@ -57,16 +69,77 @@ function App() {
   ];
 
   return (
-    <DeckGL
-      initialViewState={INITIAL_VIEW_STATE}
-      controller={true}
-      layers={layers}
+    <div
+      className="container"
+      style={{ height: "100vh", width: "100vw", padding: 0, margin: 0 }}
     >
-      <StaticMap
-        mapboxApiAccessToken={MAPBOX_TOKEN}
-        mapStyle="mapbox://styles/mapbox/light-v9"
-      />
-    </DeckGL>
+      <div
+        style={{
+          backgroundColor: "white",
+          zIndex: "1000",
+          position: "absolute",
+          marginTop: "1.0%",
+          right: "1.5%",
+          display: "flex",
+          flexDirection: "column",
+          height: "380px",
+          width: "340px",
+          paddingLeft: "20px",
+          paddingRight: "20px",
+          paddingTop: "12px",
+        }}
+      >
+        <Title level={4}>Bloom Early Detection</Title>
+        <Text>An investigation in bloom early prediction.</Text>
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "row",
+            alignItems: "center",
+            justifyContent: "space-evenly",
+            paddingTop: "16px",
+          }}
+        >
+          {heatmapColorRange.map((color) => (
+            <div
+              style={{
+                width: "16.6667%",
+                height: "18px",
+                backgroundColor: `rgb(${color[0]}, ${color[1]}, ${color[2]})`,
+              }}
+            />
+          ))}
+        </div>
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "row",
+            alignItems: "center",
+            justifyContent: "space-between",
+            paddingTop: "6px",
+          }}
+        >
+          <Text>No risk</Text>
+          <Text>Risk of HAB</Text>
+        </div>
+      </div>
+      <div
+        style={{
+          zIndex: "-1",
+        }}
+      >
+        <DeckGL
+          initialViewState={INITIAL_VIEW_STATE}
+          controller={true}
+          layers={layers}
+        >
+          <StaticMap
+            mapboxApiAccessToken={MAPBOX_TOKEN}
+            mapStyle="mapbox://styles/mapbox/dark-v9"
+          />
+        </DeckGL>
+      </div>
+    </div>
   );
 }
 
