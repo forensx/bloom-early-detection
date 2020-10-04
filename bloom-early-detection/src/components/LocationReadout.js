89d72c28928d0315a065c2b2ea0af0967feff7d6
Katsuki <katsuki@katsuki.xyz>
2020-10-04 18:51:55 -0500

    Date parsing

--- bloom-early-detection/src/components/LocationReadout.js
+++ bloom-early-detection/src/components/LocationReadout.js
@@ -14,20 +14,13 @@ import {
 const { Title, Text } = Typography;
 
 function LocationSpecific(props) {
-  const data = [
-    { x: 0, y: 8 },
-    { x: 1, y: 5 },
-    { x: 2, y: 4 },
-    { x: 3, y: 9 },
-    { x: 4, y: 1 },
-    { x: 5, y: 7 },
-    { x: 6, y: 6 },
-    { x: 7, y: 3 },
-    { x: 8, y: 2 },
-    { x: 9, y: 0 },
-  ];
   const { location } = props;
-  console.log(location);
+  let date_timeseries = [];
+  let timeseries = location.properties.TIME_SERIES;
+  for (let i = 0; i < location.properties.TIME_SERIES.length; i++) {
+    date_timeseries.push({ x: new Date(timeseries[i].x), y: timeseries[i].y });
+  }
+  location.properties.TIME_SERIES = date_timeseries;
   return (
     <div>
       <Title level={4}>{location.properties.name}</Title>
@@ -75,12 +68,17 @@ function LocationSpecific(props) {
           />
         </Col>
       </Row>
-      <XYPlot height={300} width={300} style={{ paddingTop: "12" }}>
+      <XYPlot
+        xType="time"
+        height={300}
+        width={300}
+        style={{ paddingTop: "12" }}
+      >
         <VerticalGridLines />
         <HorizontalGridLines />
         <XAxis />
         <YAxis />
-        <LineSeries data={data} />
+        <LineSeries data={location.properties.TIME_SERIES} />
       </XYPlot>
     </div>
   );
