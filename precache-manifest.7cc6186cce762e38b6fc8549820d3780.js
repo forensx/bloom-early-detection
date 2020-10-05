c92954fece55cad2d8af773ee5a98547fcd36338
Aniket Pant <aniketpant@jefcoed.com>
2020-10-04 23:39:06 -0400

    Updates

---
+++ precache-manifest.7cc6186cce762e38b6fc8549820d3780.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "704d3e758f6c2884eaa177f389c0f31f",
+    "url": "/bloom-early-detection/index.html"
+  },
+  {
+    "revision": "69eaad08dc945d1bcbb2",
+    "url": "/bloom-early-detection/static/css/2.a4459bab.chunk.css"
+  },
+  {
+    "revision": "4be0225dbb7ff827753c",
+    "url": "/bloom-early-detection/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "69eaad08dc945d1bcbb2",
+    "url": "/bloom-early-detection/static/js/2.f3d8f29d.chunk.js"
+  },
+  {
+    "revision": "ebec6ec5880fcec562f9a7747ae47b51",
+    "url": "/bloom-early-detection/static/js/2.f3d8f29d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4be0225dbb7ff827753c",
+    "url": "/bloom-early-detection/static/js/main.4592a622.chunk.js"
+  },
+  {
+    "revision": "7d7e9069299d938b4c4a",
+    "url": "/bloom-early-detection/static/js/runtime-main.2c7ad8c8.js"
+  }
+]);
